227e080e6a705db1c1aaf126df8a150d367ba2ac
Kaushik <kaushik13k@Apples-MacBook-Air.local>
2024-07-15 22:59:51 +0530

    fix

---
+++ Hello World/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Variable define
+    int age = 25;
+    cout << "My age is :" << age << endl;
+
+    // Declaration
+    int year;
+    cout << year << endl; // this will print garbage value
+
+    // Defination 
+    int year = 2024;
+
+    // int
+    int a = 10;
+
+    // float
+    float b = 10.5;
+
+    // char
+    char c = 'A';
+
+    // double
+    double d = 30.5;
+
+    // bool
+    bool isChild = true;
+    bool isChild = false;
+    bool isSchool = 0;
+    bool isSchool = 1;
+    return 0;
+}
